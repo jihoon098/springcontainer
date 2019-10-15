15374563cc45f2313bfbe96545aababa04fb91e5
BIT <BIT@DESKTOP-1KCBVQ0>
2019-10-15 21:21:21 +0900

    java + java, Java + xml mixing config

---
+++ src/main/java/config/videosystem/DVDPlayerConfig2.java
@@ -0,0 +1,16 @@
+package config.videosystem;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import kr.co.itcen.springcontainer.videosystem.DVDPlayer;
+import kr.co.itcen.springcontainer.videosystem.DigitalVideoDisc;
+
+@Configuration
+public class DVDPlayerConfig2 {
+	@Bean
+	public DVDPlayer dvdPlayer(@Qualifier("avengersInfinityWar") DigitalVideoDisc dvd) {
+		return new DVDPlayer(dvd);
+	}
+}
